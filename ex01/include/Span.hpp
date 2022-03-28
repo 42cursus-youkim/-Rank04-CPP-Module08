6ce796b62f47b9c4ca15d299dfaa3fceb3c072af
scarf <greenscarf005@gmail.com>
2022-03-28 17:47:07 +0900

    feat: :sparkles: features done

---
+++ ex01/include/Span.hpp
@@ -0,0 +1,37 @@
+#ifndef __SPAN_H__
+#define __SPAN_H__
+
+#include <algorithm>
+#include <vector>
+
+class Span {
+ private:
+  uint _n;
+  std::vector<int> _vec;
+
+  // Disabled Constructor
+  Span();
+
+  // Methods
+  void assertNotFull() const;
+
+ public:
+  // Constructors & Destructor
+  Span(uint n);
+  Span(const Span& other);
+  ~Span();
+
+  // Operators
+  Span& operator=(const Span& other);
+
+  // Methods
+  typedef std::vector<int>::iterator vec_it;
+  void addNumber(int n);
+  void addNumber(const vec_it& begin, const vec_it& end);
+
+  size_t shortestSpan() const;
+  size_t longestSpan() const;
+};
+
+
+#endif  // __SPAN_H__
---
+++ ex01/src/Span.cpp
@@ -0,0 +1,47 @@
+#include "Span.hpp"
+#include <limits>
+#include <numeric>
+#include <stdexcept>
+
+// Constructors & Destructor
+Span::Span(uint n) : _n(n) {}
+Span::Span(const Span& other) : _n(other._n), _vec(other._vec) {}
+Span::~Span() {}
+
+// Methods
+void Span::assertNotFull() const {
+  if (_vec.size() >= _n)
+    throw std::out_of_range("internal storage full");
+}
+
+void Span::addNumber(int n) {
+  assertNotFull();
+  _vec.push_back(n);
+}
+
+void Span::addNumber(const vec_it& begin, const vec_it& end) {
+  const uint size = std::distance(begin, end);
+  if (size > _n)
+    throw std::out_of_range("too many numbers");
+  _vec.insert(_vec.end(), begin, end);
+}
+
+size_t Span::shortestSpan() const {
+  if (_vec.size() < 2)
+    throw std::out_of_range("not enough numbers");
+
+  std::vector<int> sorted = _vec;
+  std::sort(sorted.begin(), sorted.end());
+  std::adjacent_difference(sorted.begin(), sorted.end(), sorted.begin());
+  size_t min = *std::min_element(sorted.begin(), sorted.end());
+  return min;
+}
+
+size_t Span::longestSpan() const {
+  if (_vec.size() < 2)
+    throw std::out_of_range("not enough numbers");
+
+  const int min = *std::min_element(_vec.begin(), _vec.end());
+  const int max = *std::max_element(_vec.begin(), _vec.end());
+  return max - min;
+}
